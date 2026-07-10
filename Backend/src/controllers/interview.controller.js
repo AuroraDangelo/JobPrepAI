117d5ca1c3cd8abcb623ab312351b06da1b16b2d
Princess <anshikapan04@gmail.com>
2026-07-11 00:45:37 +0530

    Add AI interview report and resume PDF generation

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,11 +1,12 @@
 const pdfParse = require("pdf-parse")
-const generateInterviewReport = require("../services/ai.service")
+const { generateInterviewReport,generateResumePdf } = require("../services/ai.service")
 const interviewReportModel = require("../models/interviewReport.model")
+
 
 async function generateInterViewReportController(req, res) {
 
-   // const resumeContent = await (new pdfParse.PDFParse(req.file.buffer)).getText()
-    
+    //const resumeContent = await (new pdfParse.PDFParse(req.file.buffer)).getText()
+    try{
    const uint8Array = new Uint8Array(req.file.buffer);
 
     const parser = new pdfParse.PDFParse(uint8Array);
@@ -19,6 +20,7 @@ async function generateInterViewReportController(req, res) {
         selfDescription,
         jobDescription
     })
+    console.log(interViewReportByAi);
 
     const interviewReport = await interviewReportModel.create({
         user: req.user.id,
@@ -32,7 +34,67 @@ async function generateInterViewReportController(req, res) {
         message: "Interview report generated successfully ",
         interviewReport
     })
+} catch(err) {
+    console.error(err);
+        res.status(500).json({
+            message: "Failed to generate interview report",
+            error: err.message
+        });
+}
 }
 
+async function getInterviewReportByIdController(req, res) {
 
-module.exports = {  generateInterViewReportController}
+    const { interviewId } = req.params
+
+    const interviewReport = await interviewReportModel.findOne({ _id: interviewId, user: req.user.id })
+
+    if(!interviewReport) {
+        return res.status(404).json({
+            message: "Interview report not found "
+        })
+    }
+
+    res.status(200).json({
+        message: "Interview report fetched successfully.",
+        interviewReport
+    })
+}
+
+async function getAllInterviewReportsController(req, res) {
+    const interviewReports = await interviewReportModel.find({ user: req.user.id }).sort({ createdAt: -1}).select("-resume -selfDescription -jobDescription -__v -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan")
+
+    res.status(200).json({
+        message: "Interview reports fetched successfully.",
+        interviewReports
+    })
+}
+
+async function generateResumePdfController(req, res) {
+    const { interviewReportId } = req.params
+
+    const interviewReport = await interviewReportModel.findById(interviewReportId)
+
+    if (!interviewReport) {
+        return res.status(404).json({
+            message: "Interview report not found."
+        })
+    }
+
+    const { resume, jobDescription, selfDescription } = interviewReport
+
+    const pdfBuffer = await generateResumePdf({ resume, jobDescription, selfDescription })
+
+    res.set({
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename=resume_${interviewReportId}.pdf`
+    })
+
+    res.send(pdfBuffer)
+}
+
+module.exports = {  
+    generateInterViewReportController, 
+    getInterviewReportByIdController ,
+    getAllInterviewReportsController,
+    generateResumePdfController }
--- Backend/src/routes/interview.routes.js
+++ Backend/src/routes/interview.routes.js
@@ -13,5 +13,21 @@ const upload = require("../middlewares/file.middleware" )
 
 interviewRouter.post("/", authMiddleware.authUser, upload.single("resume"), interviewController.generateInterViewReportController )
 
+/**
+ * @route GET /api/interview/report/:interviewId
+ * @description get interview report by intervireId
+ * @access private
+ */
+
+interviewRouter.get("/report/:interviewId", authMiddleware.authUser, interviewController.getInterviewReportByIdController)
+
+/**
+ * @route GET /api/interview/
+ * @description get all interview reports of logged in user
+ * @access private
+ */
+
+interviewRouter.get("/", authMiddleware.authUser, interviewController.getAllInterviewReportsController)
+
 
 module.exports = interviewRouter
