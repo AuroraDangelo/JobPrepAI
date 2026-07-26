0ff8bc97789017c5265072b9990c4537fef31a1e
ANSHIKA PANDEY <anshikapan04@gmail.com>
2026-07-26 18:03:42 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app =express()
 app.use(express.json()) //middleware ,basically it converts JSON into Javascripts objects
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: "https://jobprepai1-rzkd.onrender.com",
     credentials: true
 }))
 
