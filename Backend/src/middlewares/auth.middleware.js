107c753291a0edc85330870ccba2e68bfe8fb7fc
ANSHIKA PANDEY <anshikapan04@gmail.com>
2026-07-31 13:16:46 +0530

    Update auth.middleware.js

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,50 +1,32 @@
 const jwt = require("jsonwebtoken")
 const tokenBlackListModel = require("../models/blacklist.model")
 
-async function authUser(req, res, next) {
-
-    console.log("=================================");
-    console.log("Cookies:", req.cookies);
-
-    const token = req.cookies.token;
-
-    console.log("Token:", token);
-
-    if (!token) {
-        console.log("NO TOKEN FOUND");
-
+async function authUser(req , res , next) {
+    const token = req.cookies.token
+    if(!token) {
+        
         return res.status(401).json({
             message: "Token not provided"
         })
     }
 
     const isTokenBlackListed = await tokenBlackListModel.findOne({
-        token
+        token 
     })
 
-    console.log("Blacklisted:", isTokenBlackListed);
-
-    if (isTokenBlackListed) {
+    if(isTokenBlackListed) {
         return res.status(401).json({
             message: "Token is invalid."
         })
     }
-
-    try {
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        console.log("Decoded:", decoded);
-
-        req.user = decoded;
-
-        next();
-
-    } catch(err) {
-
-        console.log("JWT ERROR:");
-        console.log(err);
-
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        
+        req.user=decoded
+        
+        next()
+    }
+    catch(err) {
         return res.status(401).json({
             message : "Invalid token"
         })
