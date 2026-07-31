c103e4d21db2a8603a6925590bfe9ca57b748eed
ANSHIKA PANDEY <anshikapan04@gmail.com>
2026-07-31 13:37:29 +0530

    Update auth.controller.js

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -43,7 +43,12 @@ async function registerUserController(req , res) {
         process.env.JWT_SECRET,
         { expiresIn: "1d" }
     )
-    res.cookie("token" , token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+})
 
     res.status(201).json({
         message: "User registered sucessfully",
